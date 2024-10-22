a13924538c6f83eee9004c350217ecdec1dcd11a
KH4L4F6 <161086669+KH4L4F6@users.noreply.github.com>
2024-10-22 23:33:04 +0100

    Update of the deck builder
    UI

--- Assets/Scripts/UI Scripts/DeckBuilder.cs
+++ Assets/Scripts/UI Scripts/DeckBuilder.cs
@@ -16,26 +16,26 @@ public class DeckBuilder : MonoBehaviour
     {
         Inventory = this.transform.Find("IHolder/Inventory").gameObject;
 
-        Inventory.SetActive(false);
-        d1 = GameObject.Find("/Player").GetComponent<Deck>();
+        Inventory.SetActive(false); // Set the inventory canvas to false when loading up the game so its not visible
+        d1 = GameObject.Find("/Player").GetComponent<Deck>(); //Get the deck
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.B))
+        if (Input.GetKeyDown(KeyCode.B)) //Condition so when they press B the inventory loads
         {
             if (Vis == true)
             {
                 Vis = false;
-                Inventory.SetActive(Vis);
+                Inventory.SetActive(Vis); //If it is already visible and the button is pressed make it not visible by setting it to false
             }
             else if (Vis == false)
             {
                 Vis = true;
-                Inventory.SetActive(Vis);
-                ViewCards();
+                Inventory.SetActive(Vis);//Make the inventory visible
+                ViewCards(); //Call the ViewCards function below
             }
         }
 
@@ -45,16 +45,13 @@ public class DeckBuilder : MonoBehaviour
     {
         foreach (Transform slot in Inventory.transform)
         {
-            Destroy(slot.gameObject);
+            Destroy(slot.gameObject); //Loop through each slot in the inventory canvas and destroy it
         }
-        for (int i = 0; i < d1.UserCardCollection.Count; i++)
+        for (int i = 0; i < d1.UserCardCollection.Count; i++) //Loop through all of user cards
         {
-            GameObject newPre = Instantiate(pref, Inventory.transform);
-            newPre.transform.localPosition = new Vector3(0, -30 * i, 0); // Adjust position if necessary
-            newPre.transform.localScale = Vector3.one;
-
-            cardText = newPre.transform.GetChild(0).gameObject.GetComponent<TMP_Text>();
-            cardText.text = d1.UserCardCollection[i].ToString();
+            GameObject newPre = Instantiate(pref, Inventory.transform);//Create the new prefab using a card sprite and inventory canvas
+            cardText = newPre.transform.GetChild(0).gameObject.GetComponent<TMP_Text>(); //Get the text component of the prefab
+            cardText.text = d1.UserCardCollection[i].ToString(); //Change the text on the prefab to information got from the user card collection deck
         }
     }
 }
