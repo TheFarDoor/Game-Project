7dd89775441e8ccbcc1f7140a6dce0b65d09735a
Enzo Liuth <thebudderadam@gmail.com>
2025-01-05 23:16:42 +0000

    Updated Card UI
    Updated Card UI with current battle card UI

--- Assets/Scripts/UI Scripts/Inventory/DeckUICard.cs
+++ Assets/Scripts/UI Scripts/Inventory/DeckUICard.cs
@@ -5,12 +5,15 @@ using TMPro;
 public class DeckUICard : MonoBehaviour, IPointerClickHandler
 {
     [Header("Card UI References")]
-    private Card cardData;
+    public Card cardData;
     private bool isInventory; // True if the card is in the inventory, false if in the deck
     private DeckBuilder deckBuilder;
 
     public TextMeshProUGUI cardNameText;  // Card name UI reference
     public TextMeshProUGUI descriptionText; // Card description UI reference
+    public TextMeshProUGUI cardDamageText; // Card damage UI reference
+    public TextMeshProUGUI cardDefenceText; // Card defence UI reference
+    public TextMeshProUGUI cardCostText; // Card cost UI reference
 
     public void SetCardData(Card card, bool inInventory, DeckBuilder builder)
     {
@@ -18,9 +21,18 @@ public class DeckUICard : MonoBehaviour, IPointerClickHandler
         isInventory = inInventory;
         deckBuilder = builder;
 
+        cardNameText = this.transform.Find("CardPanel/MonsterName").GetComponent<TextMeshProUGUI>();
+        descriptionText = this.transform.Find("CardPanel/MonsterDesc").GetComponent<TextMeshProUGUI>();
+        cardDamageText = this.transform.Find("CardPanel/MonsterAtk").GetComponent<TextMeshProUGUI>();
+        cardDefenceText = this.transform.Find("CardPanel/MonsterDef").GetComponent<TextMeshProUGUI>();
+        cardCostText = this.transform.Find("CardPanel/MonsterCost").GetComponent<TextMeshProUGUI>();
+
         // Update UI with card information
         cardNameText.text = card.CardName;
         descriptionText.text = card.Description;
+        cardDamageText.text = "DMG: " + card.Damage.ToString();
+        cardDefenceText.text = "DEF: " + card.Health.ToString();
+        cardCostText.text = "MANA: " + card.Cost.ToString();
     }
 
     // Handle card click event
